e15707f6359a25e41f33809f796720a2a8672a5f
bricefriha <contact@bricefriha.dev>
2024-07-30 16:41:46 +0100

    Project - Create | craft gitignore

--- src/services/project.ts
+++ src/services/project.ts
@@ -11,13 +11,18 @@ export async function Create(name: string): Promise<string> {
     const gitignore = await Deno.create(`${projectDir}\\.gitignore`);
 
     // Wirte the config file
-    const writer = file.writable.getWriter();
-    await writer.write(new TextEncoder().encode(JSON.stringify({
+    const writerNest = file.writable.getWriter();
+    await writerNest.write(new TextEncoder().encode(JSON.stringify({
         modules: [
             { "thesesNuts": 4.4 },
         ],
     })));
-    await writer.close();
+    await writerNest.close();
+
+    // Wirte the config file
+    const writerGitignore = gitignore.writable.getWriter();
+    await writerGitignore.write(new TextEncoder().encode("dist/*"));
+    await writerGitignore.close();
 
     return projectDir;
 }
