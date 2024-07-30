d35a66826c3bf1c1f5973cbe2e2ec0997ce4fb61
bricefriha <contact@bricefriha.dev>
2024-07-30 15:37:00 +0100

    Add --create command

--- cli.ts
+++ cli.ts
@@ -1,5 +1,53 @@
 function Main() {
     const Args = Deno.args;
-    console.log(Args);
+    if (Args[0] === "--create") {
+        let name: string = "";
+        const defaultName: string = "IDontKnowWhatNameToGiveSoThereYouHaveIt";
+        if (Args[1])
+            name = Args[1];
+        
+        if (name) {
+            Create(name);
+            return;
+        }
+
+        if (!name|| !Args[1]?.includes("--"))
+            name = prompt("Please, enter a name for your project: ")??"";
+        if (name) {
+            Create(name);
+            return
+        }
+        if (defaultName)
+            Create(defaultName)
+    }
+}
+function Add()
+{
+    // Get current directory
+    const workDir = Deno.cwd();
+}
+/**
+ * Create a project
+ * @param name name of the project
+ */
+async function Create(name : string) : Promise<string>
+{
+    // Get current directory
+    const workDir = Deno.cwd();
+    const projectDir = `${workDir}\\${name}`;
+    await Deno.mkdir(projectDir);
+    const file = await Deno.create(`${projectDir}\\nest.json`);
+    const gitignore = await Deno.create(`${projectDir}\\.gitignore`);
+
+    // Wirte the config file 
+    const writer = file.writable.getWriter();
+    await writer.write(new TextEncoder().encode(JSON.stringify({
+        modules: [
+            { "thesesNuts": 4.4 }
+        ]
+    })));
+    await writer.close();
+
+    return projectDir;
 }
 Main();
